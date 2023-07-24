7f19c68f4fcef5c41a2360b8707cf892f1f2dc7f
ubiies <alal55al9@naver.com>
2023-07-24 20:04:08 +0900

    feat : PostControllerTest

---
+++ src/test/java/com/blog/controller/PostControllerTest.java
@@ -0,0 +1,12 @@
+package com.blog.controller;
+
+import org.junit.jupiter.api.AfterEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostControllerTest {
+
+    @AfterEach
+    void tearDown() {
+    }
+}
